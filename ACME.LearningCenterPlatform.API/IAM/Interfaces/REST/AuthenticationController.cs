17add63b5b44cd57394c68cfa33cbafcdd79bc30
teocchiii <u202211212@upc.edu.pe>
2024-11-05 20:57:01 -0500

    feat(iam): add authentication controller.

--- ACME.LearningCenterPlatform.API/IAM/Interfaces/REST/AuthenticationController.cs
+++ ACME.LearningCenterPlatform.API/IAM/Interfaces/REST/AuthenticationController.cs
@@ -1,6 +1,44 @@
+using System.Net.Mime;
+using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Services;
+using ACME.LearningCenterPlatform.API.IAM.Interfaces.REST.Resources;
+using ACME.LearningCenterPlatform.API.IAM.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+
 namespace ACME.LearningCenterPlatform.API.IAM.Interfaces.REST;
 
-public class AuthenticationController
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available authentication Endpoints")]
+
+public class AuthenticationController(IUserCommandService userCommandService) : ControllerBase
 {
-    
+    [HttpPost("sign-in")]
+    [SwaggerOperation(
+        Summary = "Sign in the user",
+        Description = "Sign in the user with the provided credentials",
+        OperationId = "SignIn")]
+    [SwaggerResponse(200, "The user is successfully signed in", typeof(AuthenticatedUserResource))]
+    [SwaggerResponse(400, "The provided credentials are invalid")]
+    public async Task<IActionResult> SignIn([FromBody] SignInResource resource)
+    {
+        var signInCommand = SignInCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var authenticatedUser = await userCommandService.Handle(signInCommand);
+        var authenticatedUserResource =
+            AuthenticatedUserResourceFromEntityAssembler.ToResourceFromEntity(authenticatedUser.user,
+                authenticatedUser.token);
+        return Ok(authenticatedUserResource);
+    }
+
+    public async Task<IActionResult> SignUp([FromBody] SignUpResource resource)
+    {
+        var signUpCommand = SignUpCommandFromResourceAssembler.ToCommandFromResource(resource);
+        await userCommandService.Handle(signUpCommand);
+        return Ok(new {message = "User is successfully signed up"});
+        
+    }
+
 }
